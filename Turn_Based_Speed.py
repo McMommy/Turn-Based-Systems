df9ebe991c7fa0366897d87ef46fde480b8f59b8
McMommy <dylanmcquade1993@gmail.com>
2025-08-21 19:57:41 -0700

    Initial commit: Turn-Based System

---
+++ Turn_Based_Speed.py
@@ -0,0 +1,35 @@
+import time
+
+
+class Player():
+    def __init__(self):
+
+        #different stat var
+        self.Speed = 100
+        self.Health = 100
+        self.Damage = 10
+        self.CritRate = 20
+        self.CritDamage = 20
+        Self.Move = ""
+
+
+
+    def PlayerTurn(self):
+        print("Your Move")
+        MoveSet()
+
+    def MoveSet(self):
+
+
+
+    def Who_Moves_First(self):
+        if Player.Speed > 100:
+            print("Guess Your faster.")
+            PlayerTurn()
+        elif Player.Speed = 100:
+            
+        elif Player.Speed < 100:
+            
+        else:
+            print("Something Went Wrong")
+
